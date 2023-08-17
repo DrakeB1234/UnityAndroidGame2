c9d7417a1080e2262e36966b0e6549e775198d99
Drake Buentello <dbuentello211607@gmail.com>
2023-08-17 16:58:34 -0500

    Started to work on the platformer assets, level design for testing purposes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] 
     private float jumpForce; 
     [SerializeField] 
+    private int jumpAmount; 
+    [SerializeField] 
     private Transform groundPos; 
     [SerializeField] 
     private LayerMask groundLayer; 
@@ -18,15 +20,32 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private float horizontalInput;
     private bool isFacingRight;
+    private int jumpRemaining;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();    
+        rb = GetComponent<Rigidbody2D>();   
+
+        Application.targetFrameRate = 60;
     }
     
     private void Update()
     {
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        if (GroundCheck())
+        {
+            // Ensure player is not in jumping state
+            playerAnimator.SetBool("isJumping", false);
+
+            // Reset player jumps
+            jumpRemaining = jumpAmount;
+        }
+        else
+        {
+            // Ensure player is in jumping state
+            playerAnimator.SetBool("isJumping", true);
+        }
     }
 
     // Input functions
@@ -40,9 +59,9 @@ public class PlayerController : MonoBehaviour
         playerAnimator.SetBool("isRunning", true);
 
         // Flip character according to input value
-        if (!isFacingRight && val == -1)
+        if (!isFacingRight && val < 0)
             FlipPlayer();
-        else if (isFacingRight && val == 1)
+        else if (isFacingRight && val > 0)
             FlipPlayer();
         // Value is 0, set animator to idle animation
         else if (val == 0)
@@ -50,15 +69,17 @@ public class PlayerController : MonoBehaviour
     }
 
     public void JumpPlayer(InputAction.CallbackContext context)
-    {
-        if (GroundCheck() && context.performed)
+    {        
+        // If the player has remaining jumps
+        if (context.started && jumpRemaining > 0)
         {
+            playerAnimator.SetTrigger("takeOff");
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpRemaining--;
         }
     }
 
     // Functions
-
     private bool GroundCheck()
     {
         return Physics2D.OverlapCircle(groundPos.position, 0.1f, groundLayer);

208569d57f3fe0011fd1810dbbec1e28cf47a750
Drake Buentello <dbuentello211607@gmail.com>
2023-08-17 13:33:29 -0500

    Added animations for jumping and running for player, added code into scripts to flip player model and switch from idle to running animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,19 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] 
     private float moveSpeed; 
+    [SerializeField] 
+    private float jumpForce; 
+    [SerializeField] 
+    private Transform groundPos; 
+    [SerializeField] 
+    private LayerMask groundLayer; 
+
+    [SerializeField] 
+    private Animator playerAnimator; 
 
     private Rigidbody2D rb;
     private float horizontalInput;
+    private bool isFacingRight;
 
     private void Awake()
     {
@@ -16,11 +26,49 @@ public class PlayerController : MonoBehaviour
     
     private void Update()
     {
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, 0f);
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
     }
+
+    // Input functions
     
     public void MovePlayer(InputAction.CallbackContext context)
     {
-        horizontalInput = context.ReadValue<Vector2>().x;
+        float val = context.ReadValue<Vector2>().x;
+        horizontalInput = val;
+
+        // Set animator to walk animation
+        playerAnimator.SetBool("isRunning", true);
+
+        // Flip character according to input value
+        if (!isFacingRight && val == -1)
+            FlipPlayer();
+        else if (isFacingRight && val == 1)
+            FlipPlayer();
+        // Value is 0, set animator to idle animation
+        else if (val == 0)
+            playerAnimator.SetBool("isRunning", false);
+    }
+
+    public void JumpPlayer(InputAction.CallbackContext context)
+    {
+        if (GroundCheck() && context.performed)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    // Functions
+
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundPos.position, 0.1f, groundLayer);
+    }
+
+    private void FlipPlayer()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
     }
 }

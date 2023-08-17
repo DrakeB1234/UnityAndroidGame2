c1a996ed240be36237a643bfbbaaf546cbab8109
Drake Buentello <dbuentello211607@gmail.com>
2023-08-17 12:19:28 -0500

    Added duck character sprite
    Separated sprite into separate body parts, working on body rigging to create player animations for walking etc...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,37 +4,23 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] 
-    public float playerSpeed; 
-    [SerializeField] 
-    private GameObject projectilePrefab; 
-    [SerializeField] 
-    private Transform firePos; 
+    private float moveSpeed; 
 
-    private Vector2 moveInput;
     private Rigidbody2D rb;
+    private float horizontalInput;
 
-    private void Awake() 
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();    
-
-        Application.targetFrameRate = 60;
     }
     
-    private void FixedUpdate() 
+    private void Update()
     {
-        rb.velocity = moveInput * playerSpeed;
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, 0f);
     }
-
+    
     public void MovePlayer(InputAction.CallbackContext context)
     {
-        Vector2 vector = context.ReadValue<Vector2>();
-
-        moveInput = vector;
-    }
-
-    public void FireWeapon(InputAction.CallbackContext context)
-    {
-        Debug.Log(context);
-        Instantiate(projectilePrefab, firePos.position, Quaternion.identity);
+        horizontalInput = context.ReadValue<Vector2>().x;
     }
 }
